8ce5754e663ad9aaec13a8a71325daed8f4eea5b
Roman Ustinov <romanustinov9@gmail.com>
2021-11-25 13:37:07 +0300

    Add project files.

---
+++ ChunkHasher.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace FileSig
+{
+    class ChunkHasher
+    {
+        public long ChunkSize { get; }
+
+        public ChunkHasher(long chunkSize)
+        {
+            ChunkSize = chunkSize;
+        }
+
+        public ParallelQuery<ChunkHash> QueryHashes(FileStream inputStream)
+        {
+            var chunks = EnumerateChunks(inputStream);
+            return QueryHashes(chunks);
+        }
+
+        private IEnumerable<(long id, Stream chunk)> EnumerateChunks(FileStream inputStream)
+        {
+            long length = inputStream.Length;
+
+            var offsets = EnumerateOffsets(length);
+
+            using (MemoryMappedFile file =
+                MemoryMappedFile.CreateFromFile(
+                    inputStream,
+                    null,
+                    0,
+                    MemoryMappedFileAccess.Read,
+                    HandleInheritability.None,
+                    false))
+            {
+                long counter = 0;
+
+                foreach (var offset in offsets)
+                {
+                    var remainig = length - offset;
+                    var chunkSize = Math.Min(remainig, ChunkSize);
+                    var mStream = file.CreateViewStream(
+                        offset,
+                        chunkSize,
+                        MemoryMappedFileAccess.Read);
+
+                    yield return (counter, mStream);
+                    
+                    counter++;
+                }
+            }
+        }
+
+        private IEnumerable<long> EnumerateOffsets(long fileLength)
+        {
+            long offset = 0;
+
+            while (offset < fileLength)
+            {
+                yield return offset;
+                offset += ChunkSize;
+            }
+        }
+
+        private ParallelQuery<ChunkHash> QueryHashes(IEnumerable<(long id, Stream buffer)> chunks)
+        {
+            return chunks
+                .AsParallel()
+                .Select(ComputeHash);
+        }
+
+        private ChunkHash ComputeHash((long id, Stream input) chunk)
+        {
+            using (var hash = SHA256.Create())
+            using (chunk.input)
+            {
+                return new ChunkHash
+                {
+                    Order = chunk.id,
+                    Hash = hash.ComputeHash(chunk.input)
+                };
+            }
+        }
+    }
+
+    struct ChunkHash
+    {
+        public long Order { get; set; }
+        public byte[] Hash { get; set; }
+
+        public override string ToString()
+        {
+            return $"Chunk: {Order}, hash: {BitConverter.ToString(Hash)}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace FileSig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = args[1];
+            long chunkSize = long.Parse(args[0]);
+
+            ChunkHasher chunkHasher = new ChunkHasher(chunkSize);
+
+            using (var input = File.OpenRead(filepath))
+            {
+                foreach (ChunkHash item in chunkHasher.QueryHashes(input))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
